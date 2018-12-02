2224a3f3eeb38e6a148ca74349bd1d94c8705749
TruongThanhNam97 <15520526@gm.uit.edu.vn>
2018-12-02 21:54:00 +0700

    Fix form sign

--- src/Components/FormBuy.js
+++ src/Components/FormBuy.js
@@ -6,22 +6,24 @@ class FormBuy extends Component {
     var script = document.createElement("script");
     script.src = "effect.js";
     head.appendChild(script);
-}
-    render() {
-        return (
-<div className="form_google" id="clicktobuy">
-  <form className="form" id="mG61Hd" name="ahihi" method="POST" action="https://docs.google.com/forms/d/e/1FAIpQLSeoZ-vPxiv9GDUKxAE5VWyFKvUlYIlJR1t0-P6PEQM0UP5HNg/formResponse">
-    <input className="info_user form_control" type="text" name="entry.2003216109" placeholder="Họ và tên" />
-    <input className="info_user" type="text" name="entry.385950496" placeholder="Số điện thoại xác nhận" />
-    <input className="info_user" type="text" name="entry.1457515468" placeholder="Mã tài khoản ví dụ (Acc #31745)" />
-    <input className="btn btn-secondary button_submit" type="submit" defaultValue="Submit" />
-    <a href="/" className="btn btn-secondary back">
-      <div className="fa fa-times" />
-    </a>
-  </form>
-</div>
-        );
-    }
+  }
+  render() {
+    return (
+      <div>
+        <div className="form_google" id="clicktobuy">
+        </div>
+        <form className="form" method="POST" action="https://docs.google.com/forms/d/e/1FAIpQLSfw9cjsb9Pu7UfC0ZNn_liyaq97EvbxYM14R6AlDYm-e8X7gA/formResponse">
+        <input className="info_user" type="text" name="entry.2110786252" placeholder="Họ và tên" />
+          <input className="info_user" type="text" name="entry.967581075" placeholder="Số điện thoại xác nhận" />
+          <input className="info_user" type="text" name="entry.630078810" placeholder="Mã tài khoản ví dụ (Acc #31745)" />
+          <input className="btn btn-secondary" type="submit" ></input>
+          <a href="/" className="btn btn-secondary back">
+            <div className="fa fa-times" />
+          </a>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default FormBuy;
