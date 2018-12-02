883f83b5242e3bd8d5b97c1cc83da6f1f53f4824
TruongThanhNam97 <15520526@gm.uit.edu.vn>
2018-12-02 17:21:48 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/lienminhshop/precache-manifest.e5a89178dfdafe206dcc1a2cb46e31a3.js"
+  "/lienminhshop/precache-manifest.7c6b77ae7316374f110aaedb90224575.js"
 );
 
 workbox.clientsClaim();
