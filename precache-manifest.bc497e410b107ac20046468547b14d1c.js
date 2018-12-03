5606beab39e1d234af6a65e9637b40aea3f789cb
TruongThanhNam97 <15520526@gm.uit.edu.vn>
2018-12-03 13:43:33 +0700

    Updates

--- precache-manifest.bc497e410b107ac20046468547b14d1c.js
+++ precache-manifest.bc497e410b107ac20046468547b14d1c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/lienminhshop/static/js/runtime~main.fa901134.js"
   },
   {
-    "revision": "47ec557dbf3d7010fe3b",
-    "url": "/lienminhshop/static/js/main.47ec557d.chunk.js"
+    "revision": "0521921b4f28a9862b62",
+    "url": "/lienminhshop/static/js/main.0521921b.chunk.js"
   },
   {
     "revision": "e8f4f8784184eaad9a94",
     "url": "/lienminhshop/static/js/1.e8f4f878.chunk.js"
   },
   {
-    "revision": "47ec557dbf3d7010fe3b",
+    "revision": "0521921b4f28a9862b62",
     "url": "/lienminhshop/static/css/main.a0f2ea79.chunk.css"
   },
   {
-    "revision": "d96e5db2dd160b8666374961c7a5d33b",
+    "revision": "7664081f3ac20c6c2402d0724b900345",
     "url": "/lienminhshop/index.html"
   }
 ];
