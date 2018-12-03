c37c8e672af52a2122d9a2943819bcee74f2fbac
TruongThanhNam97 <15520526@gm.uit.edu.vn>
2018-12-03 11:26:30 +0700

    Delete script

--- src/Components/ButtonUp.js
+++ src/Components/ButtonUp.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 
 class ButtonUp extends Component {
-    componentWillMount() {
-        var head = document.getElementsByTagName("head")[0];
-        var script = document.createElement("script");
-        script.src = "effect.js";
-        head.appendChild(script);
-      }
     render() {
         return (
 <a href="/" className="btn btn-success btn_up">
--- src/Components/ConductBuyAccount.js
+++ src/Components/ConductBuyAccount.js
@@ -25,27 +25,7 @@ class ConductBuyAccount extends Component {
               </a>
             </h5>
           </div>
-          {/* <div id="section1ContentId" class="collapse in" role="tabpanel" aria-labelledby="section1HeaderId">
-                      <div class="card-body">
-                          <p>Có 2 các đăng nhập : đăng nhập bằng Facebook và đăng nhập bằng google. ( chọn cách nào cũng được )</p>
-                      </div>
-                  </div> */}
         </div>
-        {/* <div class="card">
-                  <div class="card-header" role="tab" id="section2HeaderId">
-                      <h5 class="mb-0">
-                          <a data-toggle="collapse" data-parent="#step" href="#section2ContentId" aria-expanded="true" aria-controls="section2ContentId" class="dropdown">
-                              Bước 2 : NẠP TIỀN VÀO TÀI KHOẢN
-                  </a>
-                      </h5>
-                  </div>
-                  <div id="section2ContentId" class="collapse in" role="tabpanel" aria-labelledby="section2HeaderId">
-                      <div class="card-body">
-                          <p>- Có 2 cách để nạp tiền vào Lienminhshop.vn</p>
-                          <p>1 : Nạp Tiền Bằng Card : Xem hướng dẫn nạp <a href="" class="taiday"> tại đây</a></p>
-                          <p>2: Nạp Tiền bằng atm : Xem hướng dẫn nạp <a href="" class="taiday">tại đây</a> </p>
-                      </div>
-                  </div> */}
       </div>
       <div className="card">
         <div className="card-header" role="tab" id="section3HeaderId">
