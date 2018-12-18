437777fdf3a096f1e5abf362eb2abc663dc80ff0
TruongThanhNam97 <15520526@gm.uit.edu.vn>
2018-12-18 19:49:34 +0700

    Updates

---
+++ precache-manifest.63fdc6a0ec570b18e25be261aaf61eb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa901134316fabc7e856",
+    "url": "/lienminhshop/static/js/runtime~main.fa901134.js"
+  },
+  {
+    "revision": "5ebf6c3faaefb95ca105",
+    "url": "/lienminhshop/static/js/main.5ebf6c3f.chunk.js"
+  },
+  {
+    "revision": "4fb4b1a8405c15610e88",
+    "url": "/lienminhshop/static/js/1.4fb4b1a8.chunk.js"
+  },
+  {
+    "revision": "5ebf6c3faaefb95ca105",
+    "url": "/lienminhshop/static/css/main.2ef5d7c6.chunk.css"
+  },
+  {
+    "revision": "409d1e72519416b86c57b04834decd7a",
+    "url": "/lienminhshop/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/lienminhshop/precache-manifest.ff814082e0a966b91642c5d412755c04.js"
+  "/lienminhshop/precache-manifest.63fdc6a0ec570b18e25be261aaf61eb0.js"
 );
 
 workbox.clientsClaim();
