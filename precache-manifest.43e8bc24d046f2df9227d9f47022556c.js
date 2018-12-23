3d3facac07eb6fb202a02508b7494e0786bdbe3f
TruongThanhNam97 <15520526@gm.uit.edu.vn>
2018-12-23 16:45:20 +0700

    Updates

--- precache-manifest.43e8bc24d046f2df9227d9f47022556c.js
+++ precache-manifest.43e8bc24d046f2df9227d9f47022556c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/lienminhshop/static/js/runtime~main.fa901134.js"
   },
   {
-    "revision": "7701bf05abbee2e05ced",
-    "url": "/lienminhshop/static/js/main.7701bf05.chunk.js"
+    "revision": "60ef3e90eef1e347ca9c",
+    "url": "/lienminhshop/static/js/main.60ef3e90.chunk.js"
   },
   {
     "revision": "4fb4b1a8405c15610e88",
     "url": "/lienminhshop/static/js/1.4fb4b1a8.chunk.js"
   },
   {
-    "revision": "7701bf05abbee2e05ced",
+    "revision": "60ef3e90eef1e347ca9c",
     "url": "/lienminhshop/static/css/main.2ef5d7c6.chunk.css"
   },
   {
-    "revision": "8beb9f0f271dae0893ae26c65058d57b",
+    "revision": "95a582729ae24d1ee89254435564905d",
     "url": "/lienminhshop/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/lienminhshop/precache-manifest.69a5004141ea8f4c5d3b3e99618517d5.js"
+  "/lienminhshop/precache-manifest.43e8bc24d046f2df9227d9f47022556c.js"
 );
 
 workbox.clientsClaim();
