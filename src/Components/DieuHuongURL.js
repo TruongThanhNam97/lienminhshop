2e81dc54adb441386adc59a65a7d6621a7d2a245
TruongThanhNam97 <15520526@gm.uit.edu.vn>
2018-12-23 16:44:36 +0700

    Test url

--- src/Components/DieuHuongURL.js
+++ src/Components/DieuHuongURL.js
@@ -14,15 +14,15 @@ class DieuHuongURL extends Component {
     render() {
         return (
             <div>
-                <Route path="/" exact component={ListAccounts}/>
-                <Route path="/listaccounts"  component={ListAccounts} />
-                <Route path="/ownershop" component={OwnerShop} />
-                <Route path="/checkskin" component={CheckSkin} />
-                <Route path="/login" component={Login} />
-                <Route path="/conductbuyaccount" component={ConductBuyAccount} />
-                <Route path="/securityaccount" component={SecurityAccount} />
-                <Route path="/question" component={Question} />
-                <Route path="/seedetail/:slug.:id.html" component={SeeDetail} />
+                <Route path="/lienminhshop/" exact component={ListAccounts}/>
+                <Route path="/lienminhshop/listaccounts"  component={ListAccounts} />
+                <Route path="/lienminhshop/ownershop" component={OwnerShop} />
+                <Route path="/lienminhshop/checkskin" component={CheckSkin} />
+                <Route path="/lienminhshop/login" component={Login} />
+                <Route path="/lienminhshop/conductbuyaccount" component={ConductBuyAccount} />
+                <Route path="/lienminhshop/securityaccount" component={SecurityAccount} />
+                <Route path="/lienminhshop/question" component={Question} />
+                <Route path="/lienminhshop/seedetail/:slug.:id.html" component={SeeDetail} />
             </div>
         );
     }
