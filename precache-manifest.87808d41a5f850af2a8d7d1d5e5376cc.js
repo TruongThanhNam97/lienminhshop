46d07512c556c08ec7c4dc720be80dad0dac034e
TruongThanhNam97 <15520526@gm.uit.edu.vn>
2018-12-28 00:53:40 +0700

    Updates

--- precache-manifest.87808d41a5f850af2a8d7d1d5e5376cc.js
+++ precache-manifest.87808d41a5f850af2a8d7d1d5e5376cc.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/lienminhshop/static/js/runtime~main.fa901134.js"
   },
   {
-    "revision": "b88e7a606e2b727510a5",
-    "url": "/lienminhshop/static/js/main.b88e7a60.chunk.js"
+    "revision": "4871bf7424a4edb3115d",
+    "url": "/lienminhshop/static/js/main.4871bf74.chunk.js"
   },
   {
     "revision": "0c15125f422b53b560c0",
     "url": "/lienminhshop/static/js/1.0c15125f.chunk.js"
   },
   {
-    "revision": "b88e7a606e2b727510a5",
+    "revision": "4871bf7424a4edb3115d",
     "url": "/lienminhshop/static/css/main.2ef5d7c6.chunk.css"
   },
   {
-    "revision": "cb9de0d0c5c9c721a2ed6c6e1ca1c017",
+    "revision": "295af0b0728e7d066fe16cb4cea92c51",
     "url": "/lienminhshop/index.html"
   }
 ];
