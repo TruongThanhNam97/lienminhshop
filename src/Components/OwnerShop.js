402a34308266a2e7a2d24fef20604d82c2e9ee99
TruongThanhNam97 <15520526@gm.uit.edu.vn>
2019-01-24 17:12:47 +0700

    Fix image

--- src/Components/OwnerShop.js
+++ src/Components/OwnerShop.js
@@ -14,25 +14,25 @@ class OwnerShop extends Component {
         <p className="owner_address">0943802073</p>
       </div>
       <div className="owner wow bounceInUp" data-wow-delay="0.5s">
-        <img src="./images/owner2.jpg" alt="for react" />
+        <img src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.15752-9/50654720_392745397967720_4103574916111007744_n.jpg?_nc_cat=109&_nc_oc=AQkJLN19SJI_6fR74VWN9S217EiDdNjzWhzT-2pBXeMPURAPC7qY1vy0WpZdT178mKuYdI5wdCmFQL3wLR1t7x9G&_nc_ht=scontent.fsgn2-4.fna&oh=8d9923474a909ba76af561254d3051e4&oe=5CB58759" alt="for react" />
         <div className="manden" />
-        <a href="https://www.facebook.com/khanhlp315" className="btn btn-info fb_owner"><div className="fa fa-facebook" /></a>
-        <p className="owner_name">Lê Phương Khanh</p>
-        <p className="owner_address">15520350</p>
+        <a href="https://www.facebook.com/truongthanhnam97" className="btn btn-info fb_owner"><div className="fa fa-facebook" /></a>
+        <p className="owner_name">Trương Thanh Nam</p>
+        <p className="owner_address">0943802073</p>
       </div>
       <div className="owner wow bounceInUp" data-wow-delay="0.7s">
-        <img src="./images/owner3.jpg" alt="for react" />
+        <img src="https://scontent.fsgn2-2.fna.fbcdn.net/v/t1.15752-9/s2048x2048/50521839_337654673504302_5655561464484724736_n.jpg?_nc_cat=102&_nc_oc=AQmzeX0RMoezmZRTFaTGUrIePw7332UOO-zBjpOFdVLcNKkcprAiO7lfJE1__okKiJQIFATDdBVoWnTqUAAlv6Ff&_nc_ht=scontent.fsgn2-2.fna&oh=aa0b691af31e1d5f12744a1525b5618e&oe=5CFA45F3" alt="for react" />
         <div className="manden" />
-        <a href="https://www.facebook.com/dangxuanduykhuong" className="btn btn-info fb_owner"><div className="fa fa-facebook" /></a>
-        <p className="owner_name">Đặng Xuân Duy Khương</p>
-        <p className="owner_address">15520389</p>
+        <a href="https://www.facebook.com/truongthanhnam97" className="btn btn-info fb_owner"><div className="fa fa-facebook" /></a>
+        <p className="owner_name">Trương Thanh Nam</p>
+        <p className="owner_address">0943802073</p>
       </div>
       <div className="owner wow bounceInUp" data-wow-delay="0.9s">
-        <img src="./images/owner4.jpg" alt="for react" />
+        <img src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.15752-9/50964395_2332760493612250_6659201480819474432_n.jpg?_nc_cat=101&_nc_oc=AQkGJBw6p9WjtMNzw9r5qdXl6CuzHCsMStltAeraFIgM429lUtzdvcl3ScZw00_urZeGoawWM-qVFhJxedcu1OOL&_nc_ht=scontent.fsgn2-4.fna&oh=2b5c4dcd2b6618a6d257625c188d2804&oe=5CF9E648" alt="for react" />
         <div className="manden" />
-        <a href="https://www.facebook.com/nguyenhoang.phuc.1213" className="btn btn-info fb_owner"><div className="fa fa-facebook" /></a>
-        <p className="owner_name">Nguyễn Hoàng Phúc</p>
-        <p className="owner_address">15520645</p>
+        <a href="https://www.facebook.com/truongthanhnam97" className="btn btn-info fb_owner"><div className="fa fa-facebook" /></a>
+        <p className="owner_name">Trương Thanh Nam</p>
+        <p className="owner_address">0943802073</p>
       </div>
     </div>
   </div>
